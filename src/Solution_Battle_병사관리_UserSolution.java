ffb712dc0a62bd3ea837e7fed4aacb9a644a45a8
홍영민 <mjdal0523@gmail.com>
2023-02-22 16:59:05 +0900

    SWEA Battle 병사관리

---
+++ src/Solution_Battle_병사관리_UserSolution.java
@@ -0,0 +1,27 @@
+class Solution_Battle_병사관리_UserSolution
+{
+	public void init()
+	{
+	}
+	
+	public void hire(int mID, int mTeam, int mScore)
+	{
+	}
+	
+	public void fire(int mID)
+	{
+	}
+
+	public void updateSoldier(int mID, int mScore)
+	{
+	}
+
+	public void updateTeam(int mTeam, int mChangeScore)
+	{
+	}
+	
+	public int bestSoldier(int mTeam)
+	{
+		return 0;
+	}
+}
