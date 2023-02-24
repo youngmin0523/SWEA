52432783ef5fe9406b61a05b0629415a6ee53493
홍영민 <mjdal0523@gmail.com>
2023-02-24 17:54:34 +0900

    SWEA Battle 동아리실관리하기

--- src/Solution_Battle_동아리실관리하기.java
+++ src/Solution_Battle_동아리실관리하기.java
@@ -2,11 +2,11 @@ import java.util.*;
 import java.io.*;
 
 public class Solution_Battle_동아리실관리하기 {
-	static int N, cnt;
+	static int N;
 	static String managers;
-	static ArrayList<List<Character>> set = new ArrayList<>();
-	static boolean[] isSelected = new boolean[4];
-	static char[] members = {'A', 'B', 'C', 'D'};
+	static int[] members = {0, 1, 2, 3};
+	static List<List<Integer>> days; 
+	static long res;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,47 +14,57 @@ public class Solution_Battle_동아리실관리하기 {
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int tc = 1; tc <= T; tc++) {
-			cnt = 0;
+			res = 1;
 			managers = br.readLine();
-			getSet(0, 0, managers.charAt(0), 'A');
-			for(int i = 1; i < managers.length(); i++) {
-				ArrayList<List<Character>> tmpSet = (ArrayList<List<Character>>) set.clone();
-				set.clear();
-				for(List<Character> subSet : tmpSet) {
-					for(char keyOwner : subSet) {
-						getSet(i, 0, managers.charAt(i), keyOwner);
-					}
-				}
+			days = new ArrayList<>();
+			
+			for(int i = 0; i < managers.length(); i++) {
+				days.add(new ArrayList<>());
+				int mask = 0 | (1<<(managers.charAt(i)-'A'));
+				set(0, mask, i);
 			}
-			sb.append("#").append(tc).append(" ").append("\n");
+			List<Integer> not = new ArrayList<>();
+			for(int i = 0 ; i < 8; i++) {
+				int tmp = days.get(1).get(i);
+				not = new ArrayList<>();
+				while(tmp != 0) {
+					not.add(tmp%2);
+					tmp /= 2;
+				}
+				for(int j = not.size()-1; j >= 0; j--) {
+					System.out.print(not.get(j));
+				}
+				System.out.println("\t" + days.get(1).get(i));
+			}
+			
+			dfs(0, days.size());
+			
+			sb.append("#").append(tc).append(" ").append(res).append("\n");
 		}
 		System.out.println(sb);
 	}
-	static void getSet(int days, int cnt, char manager, char keyOwner) {
-		if(cnt == 4) {
-			if(days == managers.length()) {
-				cnt++;
-			}
-			else {
-				List<Character> tmpList = new ArrayList<>();
-				for(int i = 0; i < 4; i++) {
-					if(isSelected[i]) {
-						tmpList.add(members[i]);
-					}
-				}
-				set.add(tmpList);
-			}
+	static void dfs(int cnt, int length) {
+		if(cnt+1 == length) {
+			res++;
 			return;
 		}
-		if(members[cnt] == manager || members[cnt] == keyOwner) {
-			isSelected[cnt] = true;
-			getSet(days+1, cnt+1, manager, keyOwner);
+		
+		for(int mask : days.get(cnt)) {
+			for(int next : days.get(cnt+1)) {
+				if((mask & next) != 0) {
+					dfs(cnt+1, length);
+				}
+			}
 		}
-		else {
-			isSelected[cnt] = true;
-			getSet(days+1, cnt+1, manager, keyOwner);
-			isSelected[cnt] = false;
-			getSet(days+1, cnt+1, manager, keyOwner);
+	}
+	static void set(int cnt, int mask, int day) {
+		if(cnt == 4) {
+			days.get(day).add(mask);
+			return;
 		}
+		if((mask & (1<<members[cnt])) == 0){
+			set(cnt+1, mask|(1<<members[cnt]), day);
+		}
+		set(cnt+1, mask, day);
 	}
 }
