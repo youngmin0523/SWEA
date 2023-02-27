f081195c19b64a100c6687e4545f0b8ebde6c87b
홍영민 <mjdal0523@gmail.com>
2023-02-27 18:00:49 +0900

    SWEA D4 7465 창용마을무리의개수

---
+++ src/Solution_D4_7465_창용마을무리의개수.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution_D4_7465_창용마을무리의개수 {
+	static int N, M, set[], res;
+	static Set<Integer> groups;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			make(N);
+			for(int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				union(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			}
+			groups = new HashSet<>();
+			int group;
+			for(int i = 1; i <= N; i++) {
+				group = find(i);
+				if(!groups.contains(group)) {
+					groups.add(group);
+					res++;
+				}
+			}
+			sb.append("#").append(tc).append(" ").append(res).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+	static void make(int n) {
+		set = new int[n+1];
+		for(int i = 1; i <= n; i++) {
+			set[i] = i;
+		}
+	}
+	static void union(int a, int b) {
+		int rootA = find(a);
+		int rootB = find(b);
+		if(rootA == rootB) return;
+		set[rootB] = rootA;
+	}
+	static int find(int a) {
+		if(set[a] == a) return a;
+		return set[a] = find(set[a]);
+	}
+}
